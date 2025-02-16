69de2eaf21d7be6a354cb71b06042dc0633faf22
G/ <g@moigneu.com>
2025-02-16 17:11:55 -0600

    Add article and handle external sites

--- app/articles/ArticlesIndex.tsx
+++ app/articles/ArticlesIndex.tsx
@@ -1,51 +1,49 @@
-import { type Metadata } from 'next'
+import { type Metadata } from "next";
 
-import { Card } from '@/_components/Card'
-import { SimpleLayout } from '@/_components/SimpleLayout'
-import { getArticles } from './get-articles'
-import { formatDate } from '@/lib/formatDate'
-import { Item } from 'nextra/normalize-pages'
-import { Link } from 'next-view-transitions'
+import { Card } from "@/_components/Card";
+import { SimpleLayout } from "@/_components/SimpleLayout";
+import { getArticles } from "./get-articles";
+import { formatDate } from "@/lib/formatDate";
+import { Item } from "nextra/normalize-pages";
+import { Link } from "next-view-transitions";
 
 function Article({ article }: { article: Item }) {
   return (
     <article className="">
-      <Link href={article.route}>
+      <Link href={article.frontMatter.url ?? article.route}>
         <Card className="">
-          <Card.Title>
-            {article.title}
-        </Card.Title>
+          <Card.Title>{article.title}</Card.Title>
+          <Card.Eyebrow
+            as="time"
+            dateTime={article.frontMatter.date}
+            className="md:hidden"
+            decorate
+          >
+            {formatDate(article.frontMatter.date)}
+          </Card.Eyebrow>
+          <Card.Description>{article.frontMatter.description}</Card.Description>
+          <Card.Cta>{article.frontMatter.cta || "Read the article"}</Card.Cta>
+        </Card>
         <Card.Eyebrow
           as="time"
           dateTime={article.frontMatter.date}
-          className="md:hidden"
-          decorate
+          className="mt-1 hidden md:block"
         >
-          {formatDate(article.frontMatter.date)}
-        </Card.Eyebrow>
-        <Card.Description>{article.frontMatter.description}</Card.Description>
-        <Card.Cta>Read article</Card.Cta>
-      </Card>
-      <Card.Eyebrow
-        as="time"
-        dateTime={article.frontMatter.date}
-        className="mt-1 hidden md:block"
-      >
           {formatDate(article.frontMatter.date)}
         </Card.Eyebrow>
       </Link>
     </article>
-  )
+  );
 }
 
 export const metadata: Metadata = {
-  title: 'Articles about infrastructure, development, sustainability, and AI',
+  title: "Articles about infrastructure, development, sustainability, and AI",
   description:
-    'All of my long-form thoughts on programming, leadership, product design, and more, collected in chronological order.',
-}
+    "All of my long-form thoughts on programming, leadership, product design, and more, collected in chronological order.",
+};
 
 export default async function ArticlesIndex() {
-  const articles = await getArticles()
+  const articles = await getArticles();
 
   return (
     <SimpleLayout
@@ -60,5 +58,5 @@ export default async function ArticlesIndex() {
         </div>
       </div>
     </SimpleLayout>
-  )
+  );
 }
---
+++ content/articles/the-dual-edge-of-artificial-intelligence-shaping-our-climate-future.md
@@ -0,0 +1,11 @@
+---
+title: "The Dual Edge of AI: Saving the World or Zapping Its Energy?"
+url: https://dev.to/gmoigneu/the-dual-edge-of-artificial-intelligence-shaping-our-climate-future-111a
+published: true
+description: Artificial Intelligence (AI) stands at the forefront of technological innovation, offering transformative potential across various sectors. Simultaneously, the pressing challenge of climate change demands innovative solutions to mitigate its effects and adapt to its consequences.
+tags: sustainability, ai, energy
+cta: Read on dev.to
+# cover_image: https://direct_url_to_image.jpg
+# Use a ratio of 100:42 for best results.
+date: 2025-02-05
+---
