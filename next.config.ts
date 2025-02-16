fd4eca1c8226ba9191b18790022ad145b3650951
G/ <g@moigneu.com>
2025-02-16 08:55:11 -0600

    Add Upsun config

--- next.config.ts
+++ next.config.ts
@@ -5,9 +5,4 @@ const withNextra = nextra({});
 // You can include other Next.js configuration options here, in addition to Nextra settings:
 export default withNextra({
   // ... Other Next.js config options
-  output: "export",
-  basePath: "",
-  images: {
-    unoptimized: true,
-  },
 });
